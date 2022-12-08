fb289d9c491446a245e52bf8996a9e834659917f
grey123son <94587086+grey123son@users.noreply.github.com>
2022-12-08 08:47:50 -0800

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDCIr3F2Hc7PkpYE0oWJGzTXRneiStgEZw",
+    authDomain: "chatapp-5b710.firebaseapp.com",
+    databaseURL: "https://chatapp-5b710-default-rtdb.firebaseio.com",
+    projectId: "chatapp-5b710",
+    storageBucket: "chatapp-5b710.appspot.com",
+    messagingSenderId: "671167886598",
+    appId: "1:671167886598:web:54337b004d1f29c86ada87"
+  };
+  firebase.initializeApp(firebaseConfig);
